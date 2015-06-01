cd7a25c3fe9927d16c764cc934c5f98305483b4a
delphikettle <delphikettle@gmail.com>
2015-06-01 13:00:11 +0300

    15_05_31_12_59 //created main test

---
+++ MathLib/src/test/java/MainTest.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+    @Override
+    protected void setUp() throws Exception {
+        System.out.println("Starting main test:");
+    }
+
+    public void testMain() {
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        System.out.println("Main test successfully passed.");
+    }
+}
